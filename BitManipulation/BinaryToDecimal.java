ba8ed583f1cb178cb1183c51a16ce8d112f0f0db
Mayank-1-0 <mayankrai368@gmail.com>
2025-03-20 21:46:09 +0530

    Bit Manipulation

---
+++ BitManipulation/BinaryToDecimal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int deci=0;
+        for(int i=0;i<s.length();i++){
+            int m=s.charAt(s.length()-i-1)-'0';
+            deci+=m*Math.pow(2,i);
+        }
+        System.out.println(deci);
+        sc.close();
+    }
+}
---
+++ BitManipulation/DecimalToBinary.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        StringBuilder sb=new StringBuilder();
+        while(n!=0){
+            if(n%2==1){
+                sb.append(1);
+            }
+            else{
+                sb.append(0);
+            }
+            n=n/2;
+
+        }
+        // String s=sb.toString();
+        // for(int i=s.length()-1;i>=0;i--){
+        //     System.out.print(s.charAt(i));
+        // }
+        System.out.println(sb.reverse().toString());
+        
+        sc.close();
+    }
+}
